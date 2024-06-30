93de72ed507880d18aab19092c909b590abf6428
ciusi <ciuffetellisilvia@gmail.com>
2024-07-01 00:36:32 +0200

    Commit intermedio
    Risoluzione errori

--- fe/src/components/ComingSoon.jsx
+++ fe/src/components/ComingSoon.jsx
@@ -4,13 +4,16 @@ import Footer from './Footer';
 
 const ComingSoon = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <div className="container mt-4">
-        <h1>Coming Soon!</h1>
-        <p>
-           Per informazioni: 
-           capstoneciuffetelli@gmail.com
+      <div className="container mx-auto px-4 py-8 text-center">
+        <h1 className="text-4xl font-bold mb-4">Coming Soon!</h1>
+        <p className="text-lg mb-8">
+          We're working hard to bring you something great! <br />
+          Contact us for more information:
+          <a href="mailto:info@silviaciuffetelli.it" className="text-blue-500 ml-2">
+            info@silviaciuffetelli.it
+          </a>
         </p>
       </div>
       <Footer />
